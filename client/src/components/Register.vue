a207af89eed963627efb59e77e960be13f3fb99c
Raul <rmg1692@hotmail.com>
2019-07-27 01:23:23 -0400

    connected frontend to server

---
+++ client/src/components/Register.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    <input v-model="email" type="email" name="email" placeholder="email"/>
+    <br>
+    <input v-model="password" type="password" name="password" placeholder="password"/>
+    <br>
+    <button @click="register">Register</button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+
+export default {
+  // name: 'Register',
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    async register () {
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log(response.data)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
